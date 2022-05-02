c87c2011dd6379cc0de3fe79a29ea1fe6440cf37
ChinakaA <chi@gmail.com>
2022-05-02 19:41:53 +0100

    created honorary

--- app/Http/Controllers/InductionController.php
+++ app/Http/Controllers/InductionController.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
-use App\Models\induction;
+use App\Models\Induction;
 
 
 
