acfc09c0aec78f45c48a8ac18a6165d063b150da
ChinakaA <akaghachinaka@gmail.com>
2023-01-29 20:42:23 +0100

    set check_certificate_uploaded to false on registration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -196,7 +196,7 @@ class AuthController extends Controller
             'exemption_amount' => $exemptionAmount,
             'paid_for_registration' => $registrationPayment,
             'registration_amount' => $registrationAmount->amount,
-            'check_certificate_uploaded' => true,
+            'check_certificate_uploaded' => false,
             //certificate_upload_required was added to handle the update of regular and conversion being the same
             //'certificate_upload_required' => 'true',
             'check_exempted' => $checkExempted,
