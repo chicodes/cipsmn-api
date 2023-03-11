d4f05fa73b5ac1d1c539c262f45bf110fae45ffe
chinakaakagha <akaghachinaka@gmail.com>
2023-03-11 21:19:12 +0100

    make getAllExamToTake returned for regular same as what is returned forconversion

--- app/Models/Dashboard.php
+++ app/Models/Dashboard.php
@@ -42,20 +42,6 @@ class Dashboard extends Model
 
         $userid = Auth::user()->id;
 
-        //$registrationPayment = Auth::user()->paid_for_registration;
-
-
-
-//                $id = Auth::user()->id;
-//            $getAllExamToTake = Exam::select()
-//                //->leftJoin("exam", "exam.id", "!=", "exam_exempt.exam_id")
-//            ->rightJoin("exam_exempt","exam_exempt.exam_id", "=", "exam.id")
-//                ->whereIn()
-//            ->where("exam_exempt.userid", "=", $id)
-//                ->get();
-
-
-
         //get exams exempted and convert to array
         $getExamExempt = ExamExempt::where('userid', $userid)
                         ->get()
@@ -70,30 +56,6 @@ class Dashboard extends Model
         $getAllExamToTake = Exam::whereIn("id", $getAllExam)
             ->get();
 
-
-        //var_export($getAllExamToTake->toArray()); exit;
-
-        //Todo you need to know wether or not the user has paid for an exam so you can grey it out
-        //step1 loop through payments table using exam name to check if userid and exam name exist if it does that means
-        //the user has not paid for that exam
-
-//        foreach($getAllExamToTake as $paymentName){
-//            //var_export($exam->name);exit;
-//            $getAllExam[] = Payment::where('type', $paymentName->name)
-//                ->where('userid', $userid)
-//                //->get();
-//                ->get()
-//                ->pluck('type')
-//            ->toArray();
-//        }
-//
-//        //$getValues = array_values($getAllExam);
-//
-//        $merge = array_add($getAllExam, 'paid', $getAllExamToTake);
-//        var_export($merge); exit;
-
-        //Todo now loop through $getAllExamToTake checking if exam name equals type in $getAllExam
-
         $convertDashboard = [
             'name' => Auth::user()->firstname." ".Auth::user()->lastname,
             'exam' => $getAllExamToTake,
@@ -106,20 +68,32 @@ class Dashboard extends Model
     public static function getRegularUserDashboard(){
 
         //this is a regular user so show all exams
-        $getAllExamToTake = Exam::all();
-
-        //$getAllPayment = Payment::all();
+        //$getAllExamToTake = Exam::all();
 
         $getAllPayment = Payment::getUserPayment();
 
-        $getExam = $getAllExamToTake;
+        $userid = Auth::user()->id;
+
+        //get exams exempted and convert to array
+        $getExamExempt = ExamExempt::where('userid', $userid)
+            ->get()
+            ->pluck('exam_id')
+            ->toArray();
+
+        //get exam-exempt thats not in exams table
+        $getAllExam = Exam::whereNotIn('id', $getExamExempt)->pluck('id')
+            ->toArray();
+
+        //get exams that has not been taken by user
+        $getAllExamToTake = Exam::whereIn("id", $getAllExam)
+            ->get();
 
         $regularPayment= Auth::user()->paid_for_regular;
         // if $regularPayment ==0 that means regular user has not made regular payment so dont show user the dashboard,
         //but if 1 that means user has made payment user can now proceed to dashboard
         $regularDashboard = [
                                 'name' => Auth::user()->firstname." ".Auth::user()->lastname,
-                                'exam' => $getExam,
+                                'exam' => $getAllExamToTake,
                                 'paid_for_regular' => $regularPayment,
                                 'payment' => $getAllPayment,
                             ];
